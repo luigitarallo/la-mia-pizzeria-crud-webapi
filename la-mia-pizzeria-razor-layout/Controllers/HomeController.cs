e005edfaa671fd8af5d6923388a2737a11b1274d
luigitarallo <tarallo.gigi@gmail.com>
2024-05-24 19:19:56 +0200

    add action to home controller for create, add form to create

--- la-mia-pizzeria-razor-layout/Controllers/HomeController.cs
+++ la-mia-pizzeria-razor-layout/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace la_mia_pizzeria_razor_layout.Controllers
             return View();
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
